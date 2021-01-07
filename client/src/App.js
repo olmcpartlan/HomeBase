8b8d7d4a5db486ec242d1f0b503246c89aa962b8
Owen McPartlan <omcpartlan@owens-MacBook-Pro.local>
2021-01-07 06:53:53 -0600

    Started styling with material-ui grid!

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,54 @@
-import react, { Component, useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import FileContainer from './Components/FileContainer';
 
-export default function App() {
-  const [elements, setElements] = useState({})
-  let apiResponse = {};
-  let apiResponded = false;
+import Grid from '@material-ui/core/Grid';
 
-  // Probably need useEffect lifecycle but no luck.
-  fetch("/boost_lib")
-    .then(res => res.json())
-    .then(res => {
-      setElements(res)
-      apiResponded = true
-    });
+import './App.css';
 
-  return (
-    <div>
-      <div>
-        {apiResponded
-          ? <p>hasn't loaded</p>
-          : <div>
-            <FileContainer files={elements} />
-          </div>
-        }
+export default class Appp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponded: false,
+      files: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("/boost_lib")
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          apiResponded: true,
+          files: res
+        })
+      )
+  }
+
+  render() {
+    return (
+      <div className='main-container'>
+        <Grid container>
+          <Grid item xs={12} className='main-header'>
+            <p>header</p>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={3} className='main-sidebar'>
+            <p>side bar</p>
+          </Grid>
+          <Grid item xs={9} className='main-content'>
+            {!this.state.apiResponded 
+              ? <p>Loading . .. </p>
+              : <FileContainer files={this.state.files}/>
+            }
+          </Grid>
+
+        </Grid>
+
+
 
       </div>
-    </div>
-  )
-
+    )
+  }
 }
